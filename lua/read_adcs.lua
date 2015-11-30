67abf8951abe57b602ec62837fcea5611df9c4a0
Jack Kolb <jkolb@berkeley.edu>
2015-11-29 17:34:20 -0800

    Add very simple but functional Lua code to read from current ADC

--- lua/read_adcs.lua
+++ lua/read_adcs.lua
@@ -1,41 +1,34 @@
 -- Code to read from the current and voltage ADCs
 require "cord"
 
-cord.new(function()
-    local PORT = 3500
-    local ADDRESS = ""
-    local Reg = require "i2creg"
+function compute_power_sample(current_reg, voltage_reg)
+    cord.new(function()
+        -- Read from current ADC
+        local current_arr = current_reg:r(0x0, 2)
+        local current_val = current_arr:get_as(storm.array.INT16_BE, 0)
 
-    local CURRENT_ADDR = 0x48
-    local VOLTAGE_ADDR = 0x4B
+        -- Read from voltage ADC
+        --local voltage_arr = voltage_reg:r(0x0, 2)
+        --local voltage_val = voltage_arr:get_as(storm.array.INT16_BE, 0)
+        --local power = current_val * voltage_val
+        print("Current ADC: " .. current_val)
+    end)
+end
+
+cord.new(function()
+    local Reg = require "i2creg"
+    local CURRENT_ADDR = 0x90
+    local VOLTAGE_ADDR = 0x96
 
     -- Initialize Current ADC in continuous mode
     local current_adc = Reg:new(storm.i2c.EXT, CURRENT_ADDR)
-    current_adc:w(0x1, {0x04, 0x83})
 
+    current_adc:w(0x1, {0x04, 0x83})
     -- Initialize Voltage ADC in continuous mode
     local voltage_adc = Reg:new(storm.i2c.EXT, VOLTAGE_ADDR)
     voltage_adc:w(0x1, {0x04, 0x83})
 
-    function read_current_sample()
-        local result_arr = current_adc:r(0x0, 2)
-        local result = storm.array.get_as(storm.array.INT16_BE, 0)
-        return result
-    end
-
-    function read_voltage_sample()
-        local result_arr = voltage_adc:r(0x0, 2)
-        local result = storm.array.get_as(storm.array.INT16_BE, 0)
-        return result
-    end
-
-    function compute_power_sample()
-        local current_val = read_current_sample()
-        local voltage_val = read_voltage_sample()
-        local power_val = current_val * voltage_val
-        print(power_val)
-    end
-
-    storm.os.invokePeriodically(5 * storm.os.SECOND, compute_power_sample)
+    storm.os.invokePeriodically(1 * storm.os.SECOND, compute_power_sample, current_adc, voltage_adc)
 end)
+
 cord.enter_loop()
